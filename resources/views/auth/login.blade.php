6748c2fbde0ad182f64d30ff998d79513f15077b
huchunguang <huchunguang123@gmail.com>
2018-06-13 18:34:01 +0800

    beautiful login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
 @extends('app')
 @section('content')
-<div class="container-fluid" style="margin-top:200px;">
+<div class="container-fluid" style="margin-top:10%;">
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
+			<div class="panel panel-primary">
 				<div class="panel-heading">Login</div>
 				<div class="panel-body">
 					@if (count($errors) > 0)
@@ -21,14 +21,14 @@
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Username</label>
+							<label class="col-md-4 control-label"><span class="regist-star">*</span>Username</label>
 							<div class="col-md-6">
 								<input type="text" class="form-control" name="UserName" value="{{ old('UserName') }}">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
+							<label class="col-md-4 control-label"><span class="regist-star">*</span>Password</label>
 							<div class="col-md-6">
 								<input type="password" class="form-control" name="Pwd">
 							</div>
@@ -37,7 +37,7 @@
 
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary" style="margin-right: 15px;">
+								<button type="submit" class="btn btn-primary">
 									Login
 								</button>
 
